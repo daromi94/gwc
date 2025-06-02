b82d5fcca987525ab4745ba6a774a2e8f326094f
daromi94 <davromero07@gmail.com>
2025-06-02 15:24:07 -0500

    feat: :sparkles: count lines, words, and characters

--- src/main.go
+++ src/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	reader := bufio.NewReader(os.Stdin)
+
+	var lines, words, characters int
+	insideWord := false
+
+	for {
+		r, _, err := reader.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Fprintf(os.Stderr, "%s: cannot read stdin: %v\n", os.Args[0], err)
+			os.Exit(74)
+		}
+
+		characters++
+
+		if r == '\n' {
+			lines++
+		}
+
+		if unicode.IsSpace(r) {
+			insideWord = false
+		} else if !insideWord {
+			insideWord = true
+			words++
+		}
+	}
+
+	fmt.Printf("%8d%8d%8d\n", lines, words, characters)
 }
